d918386286a2576f24d5d3a2e84d5b9f77c48b32
Vladimir814 <vladimir_814@ukr.net>
2022-07-07 10:17:44 +0300

    Add strucks Cat,Dog and Caw

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import "fmt"
+
+const (
+	catEatsPerMonth = 7
+    dogEatsPerMonth = 10
+	cawEatsPerMonth = 25
+)
+
+type Cat struct {
+	Nickname string
+	Weight int
+}
+
+func (c Cat) getQuantityMeal() int {
+	return c.Weight * catEatsPerMonth
+}
+
+func (c Cat) getNickname() string {
+	return c.Nickname
+}
+
+func (c Cat) getWeight() int {
+	return c.Weight
+}
+
+func (c Cat) getType() string {
+	return "cat"
+}
+
+type Caw struct {
+	Nickname string
+	Weight int
+}
+
+func (cw Caw) getQuantityMeal() int {
+	return cw.Weight * catEatsPerMonth
+}
+
+func (cw Caw) getNickname() string {
+	return cw.Nickname
+}
+
+func (cw Caw) getWeight() int {
+	return cw.Weight
+}
+
+func (cw Caw) getType() string {
+	return "caw"
+}
+type Dog struct {
+	Nickname string
+	Weight int
+}
+
+func (d Dog) getQuantityMeal() int {
+	return d.Weight * dogEatsPerMonth
+}
+
+func (d Dog) getNickname() string {
+	return d.Nickname
+}
+
+func (d Dog) getWeight() int {
+	return d.Weight
+}
+
+func (d Dog) getType() string {
+	return "dog"
+}
